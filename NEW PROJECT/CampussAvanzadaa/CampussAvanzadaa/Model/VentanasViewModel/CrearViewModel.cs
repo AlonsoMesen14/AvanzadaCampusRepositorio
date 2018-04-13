4ab5ad0764d1312419ff08cfb2c74d2f8927918d
H4rr0 <haroldracing@gmail.com>
2018-04-12 23:42:29 -0600

    Model/VentanasViewModel/CrearViewModel.cs

---
+++ NEW PROJECT/CampussAvanzadaa/CampussAvanzadaa/Model/VentanasViewModel/CrearViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampussAvanzadaa.Model.VentanasViewModel
+{
+    public class CrearViewModel
+    {
+        [Display(Name = "Nombre Ventana")] 
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Codigo Ventana")]
+		public string IdVentana { get; set; }
+
+        public ICollection<VentanasXperfil> VentanasXperfil { get; set; }
+    }
+}
