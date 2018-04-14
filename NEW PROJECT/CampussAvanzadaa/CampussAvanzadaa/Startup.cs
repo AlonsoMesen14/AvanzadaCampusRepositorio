2b923508682c19b3313fdb887af0e4087d1b3c4b
Jesus Martinez <yisustkd18@gmail.com>
2018-04-13 21:56:02 -0600

    Home Controller and Registro added each one with the views

--- NEW PROJECT/CampussAvanzadaa/CampussAvanzadaa/Startup.cs
+++ NEW PROJECT/CampussAvanzadaa/CampussAvanzadaa/Startup.cs
@@ -53,19 +53,12 @@ namespace CampussAvanzadaa
             {
                 routes.MapRoute(
                     name: "default",
-<<<<<<< HEAD
+
                     template: "{controller=Home}/{action=Index}/{id?}");
-=======
-<<<<<<< HEAD
-                    template: "{controller=Notas}/{action=Edit}/{id?}");
-            });
-=======
-                    template: "{controller=Carreras}/{action=Edit}/{id?}");
->>>>>>> 122ce515403553d678962b9e5ae4ee557eeac23d
+
           //  template: "{controller=Persona_por_tipo}/{action=Create}/{id?}");
 
         });
->>>>>>> ac0d4326e00fb1400f06265db9df9a4169b9ae9f
         }
     }
 }
