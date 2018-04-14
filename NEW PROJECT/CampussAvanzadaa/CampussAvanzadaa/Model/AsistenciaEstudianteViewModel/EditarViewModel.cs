f6fdb2f2971103393ebf620e7cea54fff350511d
AlonsoMesen14 <totomesen@hotmail.com>
2018-04-13 23:50:52 -0600

    Asistencia Estudiante

--- NEW PROJECT/CampussAvanzadaa/CampussAvanzadaa/Model/AsistenciaEstudianteViewModel/EditarViewModel.cs
+++ NEW PROJECT/CampussAvanzadaa/CampussAvanzadaa/Model/AsistenciaEstudianteViewModel/EditarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace CampussAvanzadaa.Model.AsistenciaEstudianteViewModel
 {
     public class EditarViewModel
     {
+        [Display(Name = "Curso")]
+        public string CursoSeleccionado { get; set; }
 
         public ICollection<Cursos> Cursos { get; set; }
+        [Required]
+        [Display(Name = "Grupo")]
+        public string GrupoSeleccionado { get; set; }
         public ICollection<Grupos> Grupos { get; set; }
 
         public ICollection<Persona> Persona { get; set; }
